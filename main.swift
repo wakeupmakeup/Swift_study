9c06c5fb4b069691e6de14264a2bb384fadb1cf3
wakeupmakeup <100953166+wakeupmakeup@users.noreply.github.com>
2022-03-05 19:26:57 +0900

    스위프트 두번째 파일.
    함수까지

---
+++ main.swift
@@ -0,0 +1,91 @@
+import Swift
+
+/*/ 함수
+ 함수 선언의 기본형태
+ func 함수이름(매개변수1이름: 변수1타입, 매개변수2이름: 매개변수2타입 ...) -> 반환타입 {
+ 함수 구현부
+ return 반환값
+ }
+ 
+ */
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+
+//반환 값이 없는 함수
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ... ) -> void {
+// 함수 구현부
+// return }
+
+func printMyName(name: String) -> Void {
+    print(name)
+}
+//이렇게 하거나 아예 생략을 할 수도 있음.
+
+func printYourName(name: String) {
+    print(name)
+}
+
+
+// 매개변수가 없는 함수
+// func 함수이름() -> 반환타입 {
+// 함수 구현부
+// return 반환값
+// }
+
+func maximumIntegerValue() -> Int {
+    return Int.max
+}
+
+// 매개변수와 반환값이 없는 함수
+// func 함수이름() -> Void {
+// 함수 구현부
+// return
+// }
+
+func hello() -> Void {
+    print("hello")
+}
+
+// func 함수이름() {
+// 함수 구현부
+// return
+// }
+
+func bye() {
+    print("bye")
+}
+
+//함수의 호출.
+
+sum(a: 3, b: 5) // 8
+
+printMyName(name: "김태경")
+
+printYourName(name: "사랑하는 당신은 참 이름이 뭐였죠?")
+
+maximumIntegerValue() // int의 최댓값
+
+hello() // hello
+
+bye() //bye
+
+//#####################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
